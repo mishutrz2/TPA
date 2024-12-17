a83fe9d3be0906662660a6778c14832fd4339482
mishutrz2 <mishutrz2@yahoo.com>
2024-12-17 21:00:39 +0200

    started RSA Asymmetric implementation

--- TPA.Api/Controllers/AuthenticationController.cs
+++ TPA.Api/Controllers/AuthenticationController.cs
@@ -6,6 +6,7 @@ using Microsoft.IdentityModel.Tokens;
 using System.ComponentModel.DataAnnotations;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using TPA.Domain.Models;
 
@@ -124,8 +125,13 @@ public class AuthenticationController : ControllerBase
             new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
         };
 
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtSettings:Secret"]));
-        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        // HMAC Symmetric
+        // var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtSettings:Secret"]));
+        // var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        // RSA Asymmetric
+        var rsaKey = RSA.Create();
+        // to be continued ...
 
         var token = new JwtSecurityToken(
             issuer: _configuration["JwtSettings:Issuer"],
